6834b103cbaba074be17001889aefd6a0f337ae0
zhou <osinx.zhou@gmail.com>
2024-01-17 21:51:56 +0800

    音流VIP

---
+++ vip/streamMusic.js
@@ -0,0 +1,35 @@
+
+/*
+#!name = 音流解锁VIP
+#!desc =  永久解锁VIP
+#!author = osinx
+#!homepage = https://github.com/osinx/Script
+#!icon = https://raw.githubusercontent.com/osinx/Script/main/icon/streamMusic.png
+
+[Script]
+http-response ^https:\/\/pay\.aqzscn\.cn\/api\/v1\/payments\/ script-path = https://raw.githubusercontent.com/osinx/Script/main/vip/streamMusic.js, requires-body = true, tag = 音流解锁VIP
+
+[Mitm]
+hostname = play.aqzscn.cn
+*/
+
+let res = JSON.parse($response.body);
+
+const url = $request.url;
+const valiorder = "/validations/orders";
+const device = "/onlinedevices";
+
+if (url.indexOf(valiorder) != -1) {
+    res.originalTransactionId = "1704038400";
+    res.originalPurchaseDate = 1704038400;
+    res.email = "";
+    res.platform = "alipay";
+} else if (url.indexOf(device) != -1) {
+    res["Authorization-Date"] = 4102358400;
+    delete res.code
+    delete res.message
+}
+
+$done({
+    body: JSON.stringify(res)
+});
