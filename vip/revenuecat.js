e15c6291b3752775d6f4dd6e3f912fdf192d6cee
zhou <zhou.osinx@gmail.com>
2024-09-28 21:02:44 +0800

    fix

--- vip/revenuecat.js
+++ vip/revenuecat.js
@@ -14,17 +14,17 @@ let jsonToUpdate = {
   "request_date_ms": 1704070861000,
   "request_date": "2024-01-01T01:01:01Z",
   "subscriber": {
-      "entitlement": {},
-      "first_seen": "2024-01-01T01:01:01Z",
-      "original_application_version": "9692",
-      "last_seen": "2024-01-01T01:01:01Z",
-      "other_purchases": {},
-      "management_url": null,
-      "subscriptions": {},
-      "entitlements": {},
-      "original_purchase_date": "2024-01-01T01:01:01Z",
-      "original_app_user_id": "70B24288-83C4-4035-B001-573285B21AE2",
-      "non_subscriptions": {}
+    "entitlement": {},
+    "first_seen": "2024-01-01T01:01:01Z",
+    "original_application_version": "9692",
+    "last_seen": "2024-01-01T01:01:01Z",
+    "other_purchases": {},
+    "management_url": null,
+    "subscriptions": {},
+    "entitlements": {},
+    "original_purchase_date": "2024-01-01T01:01:01Z",
+    "original_app_user_id": "70B24288-83C4-4035-B001-573285B21AE2",
+    "non_subscriptions": {}
   }
 };
 
@@ -42,31 +42,33 @@ let jsonToUpdate = {
   for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapping)) {
     const productIdentifier = productInfo.product_identifier;
     const entitlements = productInfo.entitlements;
-  
+
+    $.log(`productIdentifier: ${productIdentifier}`)
+
     for (const entitlement of entitlements) {
       jsonToUpdate.subscriber.entitlements[entitlement] = {
         "purchase_date": "2024-01-01T01:01:01Z",
         "original_purchase_date": "2024-01-01T01:01:01Z",
         "expires_date": "9692-01-01T01:01:01Z",
-        "is_sandbox" : false,
+        "is_sandbox": false,
         "ownership_type": "PURCHASED",
         "store": "app_store",
         "product_identifier": productIdentifier
       };
-  
+
       // Add product identifier to subscriptions
       jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
         "expires_date": "9692-01-01T01:01:01Z",
         "original_purchase_date": "2024-01-01T01:01:01Z",
         "purchase_date": "2024-01-01T01:01:01Z",
-        "is_sandbox" : false,
+        "is_sandbox": false,
         "ownership_type": "PURCHASED",
         "store": "app_store"
       };
     }
   }
 
-  $done({body: JSON.stringify(jsonToUpdate)});
+  $done({ body: JSON.stringify(jsonToUpdate) });
 
 })()
   .catch((e) => $.logErr(e))
