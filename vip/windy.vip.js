3d02ffddcafd9b729594522e5349bba4be222af6
zhou <zhou.osinx@gmail.com>
2025-04-25 21:23:55 +0800

    windy

--- vip/windy.vip.js
+++ vip/windy.vip.js
@@ -1,26 +1,17 @@
 let response = JSON.parse($response.body);
 
-response = {
-  userInfo: {
-    email: 'admin@example.com',
-    id: 9368422,
-    username: 'jsjiami_user',
-    userslug: 'jsjiami_user_slug'
-  },
-  message: 'ok',
-  subscriptionInfo: {
-    status: 'active',
-    isTrial: false,
-    platform: 'ios',
-    tier: 'premium',
-    expiresAt: 0x444ab384d68,
-    state: 'ok',
-    isSubscription: true
-  },
-  subscription: 'premium',
-  auth: false,
-  token: ''
+response.subscriptionInfo = {
+  status: 'active',
+  isTrial: false,
+  platform: 'ios',
+  tier: 'premium',
+  expiresAt: 0x444ab384d68,
+  state: 'ok',
+  isSubscription: true
 };
+
+response.subscription = 'premium';
+response.auth = true;
 
 $done({ body: JSON.stringify(response) });
 
